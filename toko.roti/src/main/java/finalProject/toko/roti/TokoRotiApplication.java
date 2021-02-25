602024712a60a26a58a136a3c6be18acfabed8b4
alafifshinchan <alafifshinchan@gmail.com>
2021-02-25 14:52:14 +0700

    Menambah dan menghapus file

---
+++ toko.roti/src/main/java/finalProject/toko/roti/TokoRotiApplication.java
@@ -0,0 +1,13 @@
+package finalProject.toko.roti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"finalProject.toko.roti"})
+public class TokoRotiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokoRotiApplication.class, args);
+	}
+
+}
---
+++ toko.roti/src/test/java/finalProject/toko/roti/ApplicationTests.java
@@ -0,0 +1,13 @@
+package finalProject.toko.roti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
